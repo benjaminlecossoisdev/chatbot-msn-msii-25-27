b5484d7ded300f8ee5882e36458e65e4e365d8ed
fbansept <bansept.franck@gmail.com>
2026-04-07 08:51:44 +0200

    remove unused import

--- src/main/java/edu/ban7/chatbotmsnmsii2527/controller/AppUserController.java
+++ src/main/java/edu/ban7/chatbotmsnmsii2527/controller/AppUserController.java
@@ -4,7 +4,6 @@ import edu.ban7.chatbotmsnmsii2527.dao.AppUserDao;
 import edu.ban7.chatbotmsnmsii2527.model.AppUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
