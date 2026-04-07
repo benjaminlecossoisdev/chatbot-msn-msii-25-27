738e05c4954cc2ecd886b44ad07f94fc4a517efc
fbansept <bansept.franck@gmail.com>
2026-04-07 14:30:34 +0200

    add tags

--- src/main/resources/data-init.sql
+++ src/main/resources/data-init.sql
@@ -7,3 +7,14 @@ INSERT INTO recipe (name, creator_id) VALUES
         ('tarte aux pommes', 1),
         ('paella', 2),
         ('croissant', 1);
+
+INSERT INTO tag (name) VALUES
+        ('sud'),
+        ('dessert'),
+        ('arachide');
+
+INSERT INTO recipe_tags (recipe_id, tags_id) VALUES
+        (1, 2),
+        (2, 1),
+        (3, 2),
+        (3, 3);
