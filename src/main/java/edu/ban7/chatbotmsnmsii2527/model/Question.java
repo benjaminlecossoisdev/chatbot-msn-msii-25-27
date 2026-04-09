0d24cd12db7452458c3c9309d5e4f878b1330e7d
Benjamin LECOSSOIS <dev.benjaminlecossois@outlook.com>
2026-04-09 14:58:31 +0200

    1. Protecting secrets and addind .env file to gitignore file 2.Creating Question Entity

---
+++ src/main/java/edu/ban7/chatbotmsnmsii2527/model/Question.java
@@ -0,0 +1,40 @@
+package edu.ban7.chatbotmsnmsii2527.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+
+    @ManyToMany
+    @JoinTable(name= "question_include_tag", joinColumns = @JoinColumn(name="question_id"), inverseJoinColumns = @JoinColumn(name="tag_id"))
+    private List<Tag>includeTags;
+
+    @ManyToMany
+    @JoinTable(name= "question_exclude_tag", joinColumns = @JoinColumn(name="question_id"), inverseJoinColumns = @JoinColumn(name="tag_id"))
+
+    private List<Tag>excludeTags;
+
+}
